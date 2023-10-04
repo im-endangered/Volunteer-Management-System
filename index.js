dd22aec0e0675035966508f209b2a171e731cece
Pankaj Bhattarai <pankajbhattarai50@gmail.com>
2023-10-04 19:53:34 +0530

    added basic express templates and folder structure

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+const express = require("express");
+const connectDB = require("./config/dbConnect");
+
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running in port ${PORT}`);
+});
